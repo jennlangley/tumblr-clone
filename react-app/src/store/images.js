1f67759a5a7aa703cb7b5cad58086a46ad6fd72b
Jennifer Langley <jlangley1019@gmail.com>
2023-08-13 16:14:43 -0400

    images store create/edit

--- react-app/src/store/images.js
+++ react-app/src/store/images.js
@@ -50,10 +50,10 @@ export default function reducer(state = initialState, action) {
             action.payload.images.forEach(image => newState[image.id] = image);
             return newState;
         case CREATE_IMAGE:
-            newState[action.payload.image.id] = action.payload.image;
+            newState[action.payload.id] = action.payload;
             return newState;
         case EDIT_IMAGE:
-            newState[action.payload.image.id] = action.payload.image;
+            newState[action.payload.id] = action.payload;
             return newState;
         default:
             return newState;
